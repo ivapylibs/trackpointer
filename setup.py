ccaf4962d90a6e916bf27521b019693b1d521b3c
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-02-17 16:17:32 -0500

    Use find_packages and add git+ dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from setuptools import setup
-setup(name='trackpointer',
-      version='1.0',
-      description='Classes implementing object tracking by giving track point or track coordinate frame.', 
-      author='IVALab',
-      packages=['trackpointer'],
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name="trackpointer",
+    version="1.0.1",
+    description="Classes implementing object tracking by giving track point or track coordinate frame.",
+    author="IVALab",
+    packages=find_packages(),
+    install_requires=[
+        "improcessor @ git+https://github.com/ivapylibs/improcessor.git",
+        "detector @ git+https://github.com/ivapylibs/detector.git",
+        "Lie @ git+https://github.com/ivapylibs/Lie.git",
+    ],
+)
