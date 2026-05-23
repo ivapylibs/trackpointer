3e1007665b967a61ff3222a8d4cea5e1370090bf
Patricio A. Vela <pvela@gatech.edu>
2026-05-23 16:14:46 -0400

    Adding setup.py / requirements for ivapy code

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ setup(
     description="Classes implementing object tracking by giving track point or track coordinate frame.",
     author="IVALab",
     packages=find_packages(),
-    install_requires=[
-        "improcessor @ git+https://github.com/ivapylibs/improcessor.git",
-        "detector @ git+https://github.com/ivapylibs/detector.git",
-        "Lie @ git+https://github.com/ivapylibs/Lie.git",
-    ],
+    install_requires=[],
 )
